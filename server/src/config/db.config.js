ad8d4f3a0d27d450f12523a0602a86b9134cc90f
KARTHIK SAI <karthiksaikolipaka185@gmail.com>
2026-08-13 13:53:01 +0530

    Fix MongoDB connectivity and resume upload flow

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,14 +1,4 @@
 const mongoose = require('mongoose');
-const dns = require('dns');
-
-/**
- * Configure DNS servers for Node.js to resolve SRV records on Windows networks reliably
- */
-try {
-    dns.setServers(['8.8.8.8', '1.1.1.1']);
-} catch (err) {
-    console.warn('[DB Config] DNS setServers warning:', err.message);
-}
 
 /**
  * Connect to MongoDB instance safely and return connection state
@@ -25,6 +15,7 @@ const connectDB = async () => {
     const options = {
         serverSelectionTimeoutMS: 10000,
         socketTimeoutMS: 45000,
+        family: 4
     };
 
     try {
